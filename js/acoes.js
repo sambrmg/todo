a8e05d03e2aa9ff4ace414560fcdfb30275b6ba6
web8382 <web8382@caelumBSB01-01>
2019-08-09 22:10:36 -0300

    localStorage

--- js/acoes.js
+++ js/acoes.js
@@ -1,39 +1,58 @@
+function converterLista(){
+    var lista = localStorage.getItem('lista');
+    if(lista != null){
+        return JSON.parse(lista);
+    }
+    return []
+}
+
+function converterListaSetItem(lista){
+    localStorage.setItem('lista', JSON.stringify(lista))
+}
+function incluirLi(item){
+    $('.lista ul').append('<li><span>' + item + '</span><button class="feito">Feito</button><button class="excluir">Excluir</button></li>');
+}
+var lista = converterLista();
+if(lista != null){
+    for (let index = 0; index < lista.length; index++) {
+        incluirLi(lista[index])
+    }
+}
+
+
 $('#btnIncluir').click(function () {
-
     var conteudoTarefa = $('#texto').val();
-
     if (conteudoTarefa == "") {
-       
-            $('#texto').addClass('tarefa-vazia ');
-    } else{
-
-        $('.lista ul').append('<li><span>' + conteudoTarefa + '</span><button class="feito">Feito</button><button class="excluir">Excluir</button></li>');
+        $('#texto').addClass('tarefa-vazia ');
+    } else {
+        var lista = converterLista();
+        lista.push(conteudoTarefa);
+        converterListaSetItem(lista);
+        incluirLi(conteudoTarefa)
         eventoExcluir();
         eventoFeito();
     }
 });
 
-    $('#texto').keyup(function(){
-        if ( $(this).val() != ""){
-            $('#texto').removeClass('tarefa-vazia ');
-        } else {
-            $('#texto').addClass('tarefa-vazia ');
-        }
-        
-    });
+$('#texto').keyup(function () {
+    if ($(this).val() != "") {
+        $('#texto').removeClass('tarefa-vazia ');
+    } else {
+        $('#texto').addClass('tarefa-vazia ');
+    }
+});
 
 function eventoExcluir() {
-        $('.excluir').click(function (event) {
-            $(event.target).parent().remove();
-        });
-
-    }
+    $('.excluir').click(function (event) {
+        $(event.target).parent().remove();
+    });
+}
 
 /**marcar como feito **/
 function eventoFeito() {
-        $('.feito').click(function (event) {
-            $(event.target).parent().find('span').addClass('marcar');
-        })
-    };
+    $('.feito').click(function (event) {
+        $(event.target).parent().find('span').addClass('marcar');
+    })
+};
 
-  
+
