a239aec5a4b2e03af4322191a1b84a0f163a5aaa
web8382 <web8382@caelumBSB01-08>
2019-08-09 21:19:34 -0300

    estilo

--- js/acoes.js
+++ js/acoes.js
@@ -1,18 +1,29 @@
-$('#btnIncluir').click(function(){
+$('#btnIncluir').click(function () {
     var conteudoTarefa = $('#texto').val();
-$('.lista ul').append('<li><span>'+conteudoTarefa+'</span><button class="feito">Feito</button><button class="excluir">Excluir</button></li>');
-eventoExcluir ();
-eventoFeito ();
-});
-function eventoExcluir (){
-    $('.excluir').click(function(event){
-        $(event.target).parent().remove(); 
-    });
 
-}
+    if (conteudoTarefa == "") {
+       
+            $('#texto').addClass('tarefa-vazia ');
+    } else{
+
+        $('.lista ul').append('<li><span>' + conteudoTarefa + '</span><button class="feito">Feito</button><button class="excluir">Excluir</button></li>');
+        eventoExcluir();
+        eventoFeito();
+    }
+});
+
+function eventoExcluir() {
+        $('.excluir').click(function (event) {
+            $(event.target).parent().remove();
+        });
+
+    }
 
 /**marcar como feito **/
-function eventoFeito (){
-$('.feito').click(function(event){
-    $( event.target ).parent().find('span').addClass('marcar');})
-};
+function eventoFeito() {
+        $('.feito').click(function (event) {
+            $(event.target).parent().find('span').addClass('marcar');
+        })
+    };
+
+  
